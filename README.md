c199a785ce32a1ad5fc85c964e613aef990f382f
Skyler Spaeth <skylerspaeth@yandall.skylerspaeth.com>
2020-06-07 19:09:42 -0500

    my first node project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Hunters first `git` repository
+
+## Sponsored by Skyler9604
+
+From [everyoneneedsinfo.com](everyoneneedsinfo.com):
+What we gotta do is we have to talk about how much simpler Batch is than Bash scripting is on Linux because Windows is superior to Linux. Just kidding Windows sucks, it can eat my large PP, Linux is where it's at.
+
+Also Hunter says that there's an Xbox One giveaway going on somewhere in the world.
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 4200;
+
+const operators = [
+	{ url: "add", doMath: (num1, num2) => { return parseFloat(num1) + parseFloat(num2) } },
+	{ url: "divide", doMath: (num1, num2) => { return num1 / num2 } },
+	{ url: "multiply", doMath: (num1, num2) => { return num1 * num2 } },
+	{ url: "subtract", doMath: (num1, num2) => { return num1 - num2} }
+]
+
+operators.forEach((e) => {
+	app.get(`/${e.url}/:num1/:num2`, (req, res) => {
+		res.send(`<h1>Results: ${e.doMath(req.params.num1, req.params.num2)}</h1>`);
+	});
+});
+
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
